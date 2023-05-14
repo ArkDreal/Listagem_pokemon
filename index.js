cf5e6b60cab8387188eacada8c4e3688ebb6dda8
ArkDreal <andrey.o.leal@gmail.com>
2023-05-14 10:19:25 -0300

    Projeto Finalizado

--- index.js
+++
@@ -1,5 +0,0 @@
-const btnNightMode = document.querySelector('#btn-night-mode');
-btnNightMode.addEventListener('click', function() {
-    const body = document.body;
-    body.classList.toggle('night-mode');
-});
